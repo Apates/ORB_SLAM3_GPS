773ee2a08e6645b9acb4404904f87bdbf7e5df70
Gruber <luk24515@VID-LT-HERMAXH1.pc.iitb.fhg.de>
2025-12-06 12:27:09 +0100

    added EdgeGPS type to g2o

---
+++ Thirdparty/g2o/g2o/types/EdgeGPS.cpp
@@ -0,0 +1,5 @@
+#include "EdgeGPS.h"
+#include <g2o/core/factory.h>
+
+G2O_REGISTER_TYPE(EDGE_GPS, EdgeGPS)
+
---
+++ Thirdparty/g2o/g2o/types/EdgeGPS.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <g2o/core/base_unary_edge.h>
+#include <g2o/types/slam3d/types_six_dof_expmap.h> // VertexSE3Expmap
+#include <Eigen/Core>
+
+using namespace g2o;
+
+class EdgeGPS : public BaseUnaryEdge<3, Eigen::Vector3d, VertexSE3Expmap>
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    EdgeGPS() = default;
+
+    void computeError() override {
+        const VertexSE3Expmap* v = static_cast<const VertexSE3Expmap*>(_vertices[0]);
+        // Extract translation from vertex estimate
+        Eigen::Vector3d t = v->estimate().translation();
+        _error = t - _measurement; // predicted - measured (we could do measured - predicted; must be consistent)
+    }
+
+    // Optional: robustify or clamp large errors if you want
+    virtual bool read(std::istream& is) { return false; }
+    virtual bool write(std::ostream& os) const { return false; }
+};
